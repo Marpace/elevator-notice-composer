7de207fec38bee898520a29bca842735d3dffa36
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-08-17 15:59:54 -0400

    install gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // enables static export
+  basePath: "/elevator-notice-composer", 
+  images: {
+    unoptimized: true, // disable Image Optimization (not supported on GitHub Pages)
+  },
+};
 
 export default nextConfig;
