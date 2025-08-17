b1822082f48ac72504f624a802fbe8305b807468
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-08-17 16:48:34 -0400

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export", // enables static export
   basePath: "/elevator-notice-composer",
   assetPrefix: "/elevator-notice-composer/",
+  trailingSlash: true,  // <--- important for GitHub Pages
   images: {
     unoptimized: true, // disable Image Optimization (not supported on GitHub Pages)
   },
